81c3368e1c2186cc55be88eb3318516e45ae89cb
Mayk913 <lomejor913@gmail.com>
2022-11-15 20:15:03 -0500

    agregando el cors

--- actividad5/Apps/clientes/views.py
+++ actividad5/Apps/clientes/views.py
@@ -2,40 +2,74 @@ from django.shortcuts import render
 from django.http import Http404
 
 from rest_framework.response import Response
-from rest_framework import generics
-from rest_framework import mixins
+from rest_framework.views import APIView
 
 from rest_framework import status
 
 from Apps.clientes.models import Cliente
 from Apps.clientes.serializer import ClienteSerializer
-class ClienteList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+
+# Create your views here.
+
+
+class ClienteList(APIView):
     """
     Lista de Clientes
     """
-  
-    queryset = Cliente.objects.all()
-    serializer_class = ClienteSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+    def get(self, request, format=None):
+        clientes = Cliente.objects.all()
+        serializer = ClienteSerializer(clientes, many=True)
+        print(clientes)
+        return Response({"clientes":serializer.data})
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-class ClienteDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+    def post(self, request, format=None):
+        serializer = ClienteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ClienteDetail(APIView):
     """
     Retrieve, update or delete de los clientes por pk
     """
-    queryset = Cliente.objects.all()
-    serializer_class = ClienteSerializer
+    def get_object(self, pk):
+        # Returns an object instance that should 
+        # be used for detail views.
+        try:
+            return Cliente.objects.get(pk=pk)
+        except Cliente.DoesNotExist:
+            raise Http404
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+    def get(self, request, pk, format=None):
+        cliente = self.get_object(pk)
+        serializer = ClienteSerializer(cliente)
+        # return Response(serializer.data)
+        return Response({"clientes":serializer.data})
 
-    def put(self, request, *args, **kwargs):
-        return self.u
+    def put(self, request, pk, format=None):
+        cliente = self.get_object(pk)
+        serializer = ClienteSerializer(cliente, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            # return Response(serializer.data)
+            return Response({"clientes":serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk, format=None):
+        cliente = self.get_object(pk)
+        serializer = ClienteSerializer(cliente,
+                                        data=request.data,
+                                        partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        cliente = self.get_object(pk)
+        cliente.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- actividad5/actividad5/settings.py
+++ actividad5/actividad5/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'Apps.proveedores.apps.ProveedoresConfig',
     'Apps.productos.apps.ProductosConfig',
     'Apps.clientes.apps.ClientesConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,6 +54,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:8000',
+)
+
 
 ROOT_URLCONF = 'actividad5.urls'
 
